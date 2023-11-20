4f48bd75fa182b41d754f9c34573d2b98b5218e3
Anatoliy <avk12061996@gmail.com>
2023-11-20 15:07:51 +0200

    Updated setup file

--- setup.py
+++ setup.py
@@ -9,6 +9,9 @@ setup(
         'grpcio-tools',
         'protobuf',
     ],
+    description='A simple client-server chat application using gRPC in Python with Etcd3 for message storage.'
+                'The server supports operations like retrieving a list of users, sending messages, '
+                'and broadcasting new messages to subscribed clients.',
     entry_points={
         'console_scripts': [
             'grpc_chat_server=grpc_chat.server:run_server',
@@ -20,4 +23,9 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
+    project_urls={
+        'Documentation': 'https://grpc.io/docs/languages/python/quickstart/',
+        'Source Code': 'https://github.com/AVK1206/GRPC_CHAT',
+    },
+
 )
