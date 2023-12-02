02dd1841a476557041c427fe070be4e8eac1de2b
Anatoliy Kalenskiy <134367028+AVK1206@users.noreply.github.com>
2023-12-02 13:17:15 +0200

    Updated server.py

--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-""" gRPC Chat Service Implementation.
+"""gRPC Chat Service Implementation.
 
 This script defines the implementation of a gRPC Chat Service using
 the provided protocol buffer messages and gRPC service definitions.
@@ -9,17 +9,19 @@ The server implements three RPC methods:
 3. Subscribe: Subscribe to a stream of chat messages.
 
 The server uses the ChatServiceServicer class to handle these methods.
+Messages are stored in etcd for persistence.
 
 To run the server, execute this script. The server will start
-on localhost:50052 by default. A client can customize
+on localhost:50053 by default. A client can customize
 the host and port by providing different values to them.
 """
 
 import grpc
+import etcd3
 from concurrent import futures
 
 from build import chat_pb2, chat_pb2_grpc
-from config import GRPC_HOST, GRPC_PORT
+from config import GRPC_HOST, GRPC_PORT, ETCD_HOST, ETCD_PORT
 
 
 class ChatServiceServicer(chat_pb2_grpc.ChatServiceServicer):
@@ -32,23 +34,36 @@ class ChatServiceServicer(chat_pb2_grpc.ChatServiceServicer):
         self.users = [chat_pb2.User(login="user1", full_name="John Wick"),
                       chat_pb2.User(login="user2", full_name="Johnny Depp")]
 
-        self.messages = []
+        self.etcd_client = etcd3.client(host=ETCD_HOST, port=ETCD_PORT)
 
     def GetUsers(self, request, context):
-        """Handles the GetUsers RPC call."""
+        """Handles the GetUsers RPC call to the current user
+        and returns all users' login and full name.
+        """
         return chat_pb2.GetUsersReply(users=self.users)
 
     def SendMessage(self, request, context):
-        """Handles the SendMessage RPC call."""
-        self.messages.append(request.message)
+        """Handles the SendMessage RPC call to the current user
+        and stores messages from him in etcd.
+        """
+        key = f"messages/{request.message.from_user}/{request.message.to_user}"
+        value = request.message.body
+        self.etcd_client.put(key, value)
         return chat_pb2.SendMessageReply()
 
     def Subscribe(self, request, context):
-        """Handles the Subscribe RPC call."""
+        """Handles the Subscribe RPC call to the current user
+        and retrieves messages for him from etcd.
+        """
         user_login = request.login
-        for message in self.messages:
-            if message.to_user == user_login:
-                yield message
+        prefix = f"messages/{user_login}/"
+        for key, value in self.etcd_client.get_prefix(prefix):
+            message = chat_pb2.Message(
+                from_user=user_login,
+                to_user=key.split("/")[-1],
+                body=value
+            )
+            yield message
 
 
 def serve(host, port):
