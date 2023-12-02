3de221e2cfe72c7ec6db33e181a34b2b463c78e3
Anatoliy Kalenskiy <134367028+AVK1206@users.noreply.github.com>
2023-12-02 13:15:54 +0200

    Updated config.py

--- config.py
+++ config.py
@@ -1,16 +1,22 @@
-""" gRPC Chat Service Configuration.
+"""gRPC Chat Service Configuration.
 
-This module sets up the configuration for the gRPC Chat Service.
-It defines constants for the gRPC server host and port based on
-environment variables. If the environment
-variables are not set, default values are used.
+This module defines constants for configuring the gRPC Chat Service.
+It sets up environment variables to customize the gRPC server
+host and port, as well as the etcd host and port for message storage.
+If the environment variables are not set, default values are used.
 
 Constants:
     GRPC_HOST (str): The gRPC server host, defaulting to "localhost".
-    GRPC_PORT (int): The gRPC server port, defaulting to 50052.
+    GRPC_PORT (int): The gRPC server port, defaulting to 50053.
+    ETCD_HOST (str): The etcd server host, defaulting to "localhost".
+    ETCD_PORT (int): The etcd server port, defaulting to 2379.
 """
 
 import os
 
+
 GRPC_HOST = os.environ.get("GRPC_CHAT_HOST", "localhost")
-GRPC_PORT = int(os.environ.get("GRPC_CHAT_PORT", 50052))
+GRPC_PORT = int(os.environ.get("GRPC_CHAT_PORT", 50053))
+
+ETCD_HOST = os.environ.get("ETCD_HOST", "localhost")
+ETCD_PORT = int(os.environ.get("ETCD_PORT", 2379))
