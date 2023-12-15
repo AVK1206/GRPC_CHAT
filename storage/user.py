e05e75eb73b729ef208ed3ee8a36371667f0daac
Anatolii Kalenskyi <anatoliikalenskyi@Anatoliis-MacBook-Pro.local>
2023-12-15 18:09:21 +0200

    Updated user

--- storage/user.py
+++ storage/user.py
@@ -1,3 +1,11 @@
+"""The module defines the User data model for the gRPC Chat application.
+
+The User class in this module represents the data structure
+for user information within the chat system. It includes
+functionality for converting between the custom User dataclass
+and the protobuf User object.
+"""
+
 from dataclasses import dataclass, asdict
 
 from build import chat_pb2
@@ -5,6 +13,7 @@ from build import chat_pb2
 
 @dataclass
 class User:
+    """A dataclass representing a user in the chat system."""
     login: str
     full_name: str
 
@@ -13,9 +22,14 @@ class User:
         return chat_pb2.User(login=self.login, full_name=self.full_name)
 
     def to_dict(self) -> dict:
-        """Convert User to dictionary"""
+        """Convert the User instance into a dictionary"""
         return asdict(self)
 
     def get_unique_key(self) -> str:
         """Get unique key for the user."""
         return self.login
+
+    @classmethod
+    def from_pb(cls, pb_obj):
+        """Create a User instance from a protobuf User object."""
+        return cls(login=pb_obj.login, full_name=pb_obj.full_name)
